cd0c4b6ac805f3a2c8bd0f7d48ecaf08a7c2482a
ctk0327 <ctk03272@naver.com>
2020-05-06 17:23:17 +0900

    my commit message

---
+++ TwoSum/ctk0327.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] answer = new int[2];
+​
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                if ( i != map.get(target - nums[i])) {
+                    answer[0] = i;
+                    answer[1] = map.get(target - nums[i]);
+                    break;
+                }
+            }
+         map.put(nums[i], i);
+        }
+        return answer;
+        
+    }
+}
