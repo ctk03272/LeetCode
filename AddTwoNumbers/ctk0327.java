b0736064aba0247eb37d67484f8fa274d5651af3
ctk0327 <ctk0327@naver.com>
2021-02-27 16:53:37 +0900

    commit~~

--- AddTwoNumbers/ctk0327.java
+++ AddTwoNumbers/ctk0327.java
@@ -8,26 +8,28 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // nochange
         int carry = 0;
         ListNode prevNode = null;
         ListNode nowNode = null;
         ListNode firstNode = null;
         while (l1 != null || l2 != null || carry!=0) {
             int val = 0;
+            
             if (getVal(l1) + getVal(l2) + carry >= 10) {
                 val = +getVal(l1) + getVal(l2) + carry - 10;
                 carry = 1;
             } else {
                 val = getVal(l1) + getVal(l2) + carry;
-                                carry=0;
+                carry=0;
             }
+            
             nowNode = new ListNode(val);
             if (prevNode != null) {
                 prevNode.next = nowNode;
             } else {
                 firstNode = nowNode;
             }
+            
             prevNode = nowNode;
             if (l1 != null) {
                 l1 = l1.next;
@@ -35,7 +37,7 @@ class Solution {
             if (l2 != null) {
                 l2 = l2.next;
             }
-                                if(l2==null && carry==0){
+            if(l2==null && carry==0){
                 prevNode.next=l1;
                 break;
             }
